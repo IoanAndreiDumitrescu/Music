2a06c1ee950939babbab93793daed8af793538f5
deiud <deiud@DESKTOP-SDS1J7U>
2024-03-17 21:22:55 +0100

    Add project files.

---
+++ LandingPage/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace LandingPage.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
