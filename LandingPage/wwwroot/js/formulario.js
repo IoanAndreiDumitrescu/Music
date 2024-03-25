59c37f0a5fdd10fbc55a6953c4f51d4bdecfdc5c
Andrei Dumitrescu <andrei.ion.dumitrescu@gmail.com>
2024-03-25 20:56:15 +0100

    update formpage: regex and rules

--- LandingPage/wwwroot/js/formulario.js
+++ LandingPage/wwwroot/js/formulario.js
@@ -1,13 +1,63 @@
-document.getElementById("birthdayDate").addEventListener("blur", function () {
-    //Test the value of the input field and whether it matches the regular expression,     where val is the value extracted from the field
-    if (!this.value.match(/^\d+\.?\d+$/)) {
-        alert("error");
-    }
-}, false);
 
-document.getElementById("phoneNumber").addEventListener("blur", function () {
-    //Test the value of the input field and whether it matches the regular expression,     where val is the value extracted from the field
-    if (!this.value.match(/^ [0 - 9] * $/)) {
-        alert("Solo numeros");
+const emailAddress = document.querySelector("#emailAddress");
+
+emailAddress.addEventListener("input", function(event) {
+    if (emailAddress.validity.typeMismatch) {
+        emailAddress.setCustomValidity("Please enter a valid email address.");
+    } else if (emailAddress.validity.valueMissing) {
+        emailAddress.setCustomValidity("Email field cannot be empty.");
+    } else {
+        emailAddress.setCustomValidity("");
     }
-}, false);
+});
+
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+
+function validateInput(input) {
+    input.addEventListener("input", function() {
+        if(input.value === "") {
+            input.setCustomValidity("This can't be empty.");
+        } else {
+            input.setCustomValidity("");
+        }
+    });
+}
+
+// Call for the firstName and lastName inputs
+validateInput(firstName);
+validateInput(lastName);
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const birthdayDate = document.querySelector("#birthdayDate");
+    birthdayDate.required = true;
+
+    // Auto-adding slashes after the date and month
+    birthdayDate.addEventListener("input", (event) => {
+        var rawValue = event.target.value;
+        if (rawValue.includes('/')) {
+            rawValue = rawValue.replace(/\D/g, '');
+        }
+        const day = rawValue.slice(0, 2);
+        const month = rawValue.slice(2, 4);
+        const year = rawValue.slice(4);
+        event.target.value = day + (month ? '/' + month + '/' + year : '');
+    });
+
+    birthdayDate.addEventListener("invalid", (event) => {
+        if (birthdayDate.validity.patternMismatch) {
+            event.target.setCustomValidity('Invalid Date Format, expected DD/MM/YYYY');
+        } else if (birthdayDate.validity.valueMissing) {
+            event.target.setCustomValidity('This field must be filled.');
+        } else {
+            event.target.setCustomValidity('');
+        }
+    });
+
+    document.querySelector('form').addEventListener("submit", function(event) {
+        if (!birthdayDate.checkValidity()) {
+            event.preventDefault();
+        }
+    });
+});
